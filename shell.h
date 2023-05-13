cbefcbc84c377ea7810a8fc91adb1888d6ba4e90
Mohamed El omary <elomarymohamed72@gmail.com>
2023-05-13 21:09:30 +0100

    feat: finish function that handles parsing

--- shell.h
+++ shell.h
@@ -1,5 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdlib.h>
@@ -8,4 +9,6 @@
 
 void *_realloc(void *old_buffer, size_t old_size, size_t new_size);
 ssize_t _getline(char **line);
+char *_trim_white_space(char *line);
+int _parsing_error_handler(char *line);
 #endif
