1771a31b5ff501db037b9da885a160ff76c0c12a
Hamza Naitsi <93101878+h01nait@users.noreply.github.com>
2023-05-14 19:22:08 +0100

    linkedlist_finished (#4)

---
+++ _free_list.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * free_list - Free memory allocated for the list.
+ * @list: Ptr to head of list.
+ * @free_content: Ptr to function for freeing node content.
+ * Return: void
+ */
+void free_list(list_t *list, void (*free_content)(void *data))
+{
+	list_t *current = list;
+	list_t  *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		if (free_content != NULL)
+		{
+			free_content(current->data);
+		}
+		free(current);
+		current = next;
+	}
+}
