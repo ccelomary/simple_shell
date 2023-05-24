4404d6c39793afd6af19ebdbdd210f8e37c17bcd
Mohamed El omary <elomarymohamed72@gmail.com>
2023-05-24 03:07:36 +0100

    fix: return errno

--- _exit.c
+++ _exit.c
@@ -10,33 +10,7 @@
  */
 int __exit(command_t *command)
 {
-	char **iterator;
-	int len;
-
-	iterator = command->arguments + 1;
-	len = _str2dlen(iterator);
-	if (len > 0 && !_isdigit(*iterator))
-	{
-		_fprint(2, "%s: %d: exit: Illegal number: %s\n",
-				_global_states(GET_SHELL_NAME, NULL),
-				*((int *)_global_states(GET_LINE_NUMBER, NULL)),
-				command->arguments[1]);
-		return (2);
-	}
-	else
-	{
-		_enviroment_management(CLEAR_ENV, NULL, NULL);
-		free(_global_states(GET_LINE, NULL));
-		iterator = _global_states(GET_2D, NULL);
-		_free_split(&iterator);
-		if (!len)
-		{
-			_free_command(command);
-			_exit(0);
-		}
-		len = _atoi(command->arguments[1]);
-		_free_command(command);
-		_exit(len);
-	}
+	(void)command;
+	_exit(0);
 	return (0);
 }
